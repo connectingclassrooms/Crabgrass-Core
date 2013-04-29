f1d4c8e15b9f619f701d21da5e0a9bf8fd0553d9
() <nilclass@riseup.net>
2013-04-29 10:42:38 +0200

    added simple 'admin' layout

--- extensions/mods/super_admin/app/controllers/admin/base_controller.rb
+++ extensions/mods/super_admin/app/controllers/admin/base_controller.rb
@@ -2,6 +2,7 @@ class Admin::BaseController < ApplicationController
   # these helpers are needed for the links added to the admin navigation by this mod
   helper 'admin/users', 'admin/groups', 'admin/memberships'
 
+  layout 'admin'
 
   prepend_before_filter :restore_superadmin, :only => :index
 
