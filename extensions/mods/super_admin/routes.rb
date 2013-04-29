d95659380d70168f218d14d6d891521cf09d688e
() <nilclass@riseup.net>
2013-04-29 10:14:32 +0200

    superadmin: use Crabgrass.mod_routes instead of regular plugin routes

--- extensions/mods/super_admin/routes.rb
+++ extensions/mods/super_admin/routes.rb
@@ -3,7 +3,8 @@ this will create the routes
   /admin/groups -> Admin::GroupsController
   /admin/users  -> Admin::UsersController
 =end
-ActionController::Routing::Routes.draw do |map|
+
+Crabgrass.mod_routes do |map|
   map.namespace :admin do |admin|
     admin.resources :groups
     admin.resources :users
