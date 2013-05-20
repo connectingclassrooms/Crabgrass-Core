abe8a31e7a68b812a17a5f6bbbe63398baa5b6fb
Azul <azul@riseup.net>
2013-05-20 20:41:12 +0200

    added initial admin dropdown menu
    it's based on the theme menu structure so we can easily extend it from other admin mods

--- extensions/themes/admin/navigation.rb
+++ extensions/themes/admin/navigation.rb
@@ -5,6 +5,22 @@ define_navigation(parent: 'default') do
     visible { may_admin_site? }
     url     '/admin'
     active  { controller?('admin/groups', 'admin/users') }
+    html    :partial => '/layouts/global/nav/admin_menu'
+
+    context_section :groups do
+      label { :groups.t }
+      url   { admin_groups_path }
+      active { controller?('admin/groups') }
+      visible { true || may_super? }
+    end
+
+    context_section :users do
+      label { :users.t }
+      url   { admin_users_path }
+      active { controller?('admin/users') }
+      visible { true || may_super? }
+    end
+
   end
 end
 
