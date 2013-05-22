f82b8af0f66597408309512616bf6418fcc7c8a3
Azul <azul@riseup.net>
2013-05-22 12:53:43 +0200

    superadmin is not friend or peer of all
    We alter the permissions with may? and may! directly. This only messes up friending and joining groups.

--- extensions/mods/super_admin/app/models/user_extension/super_admin.rb
+++ extensions/mods/super_admin/app/models/user_extension/super_admin.rb
@@ -4,10 +4,6 @@ module UserExtension::SuperAdmin
     base.extend(ClassMethods)
     base.instance_eval do
       include InstanceMethods
-      alias_method_chain :member_of?, :superadmin
-      alias_method_chain :direct_member_of?, :superadmin
-      alias_method_chain :friend_of?, :superadmin
-      alias_method_chain :peer_of?, :superadmin
       alias_method_chain :may!, :superadmin
       alias_method_chain :may?, :superadmin
     end
@@ -33,27 +29,6 @@ module UserExtension::SuperAdmin
       else
         false
       end
-    end
-
-    def member_of_with_superadmin?(group)
-      return true if superadmin?
-      return member_of_without_superadmin?(group)
-    end
-
-    # is the user a direct member of the group?
-    def direct_member_of_with_superadmin?(group)
-      return true if superadmin?
-      return direct_member_of_without_superadmin?(group)
-    end
-
-    def friend_of_with_superadmin?(user)
-      return true if (superadmin? or user.superadmin?)
-      return friend_of_without_superadmin?(user)
-    end
-
-    def peer_of_with_superadmin?(user)
-      return true if (superadmin? or user.superadmin?)
-      return peer_of_without_superadmin?(user)
     end
 
     def may_with_superadmin!(perm, object)
