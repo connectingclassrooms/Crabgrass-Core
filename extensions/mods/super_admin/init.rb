9088049d500b835cdf554ad23be0aa1b55b0f54d
Azul <azul@riseup.net>
2013-05-23 11:20:50 +0200

    use ActiveSupport Concerns to extend controllers.

--- extensions/mods/super_admin/init.rb
+++ extensions/mods/super_admin/init.rb
@@ -11,7 +11,7 @@ extend_model :User do
   include Crabgrass::SuperAdmin::Extends::User
 end
 
-extend_model 'ApplicationController' do
+Common::Application::BeforeFilters.module_eval do
   include Crabgrass::SuperAdmin::ModifyTheme
 end
 
