ab58dae8ac6ab8169f1b050c3b1f36c98cf1b363
Azul <azul@riseup.net>
2013-05-24 10:52:01 +0200

    apply superadmin modifications on prepare not when including the plugin

--- extensions/mods/super_admin/init.rb
+++ extensions/mods/super_admin/init.rb
@@ -3,15 +3,15 @@ self.load_once = false
 
 Rails.application.config.to_prepare do
   #require 'super_admin_listener'
+
+  require File.expand_path('routes', File.dirname(__FILE__))
+
+  extend_model :User do
+    include Crabgrass::SuperAdmin::Extends::User
+  end
+
+  Common::Application::BeforeFilters.module_eval do
+    include Crabgrass::SuperAdmin::ModifyTheme
+  end
+
 end
-
-require File.expand_path('routes', File.dirname(__FILE__))
-
-extend_model :User do
-  include Crabgrass::SuperAdmin::Extends::User
-end
-
-Common::Application::BeforeFilters.module_eval do
-  include Crabgrass::SuperAdmin::ModifyTheme
-end
-
